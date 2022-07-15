2ad953d214089ecd30fe5d994197eceaaefaa5c2
Andrew Ratnikov <ratnikov.am@gmail.com>
2022-07-15 19:09:36 +0300

    add simple routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
 
+import Login from './pages/Login';
+import Sensors from './pages/Sensors';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to='login' replace />} />
+        <Route path='login' element={<Login />} />
+        <Route path='sensors' element={<Sensors />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
